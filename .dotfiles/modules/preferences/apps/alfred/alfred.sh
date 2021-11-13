09abc7fa99afea01b3ba0e4e2f32f9fa60841625
Nicholas Adamou <nicholasadamouemail@gmail.com>
2021-11-13 16:35:39 -0500

    fixed bugs

--- .dotfiles/modules/preferences/apps/alfred/alfred.sh
+++ .dotfiles/modules/preferences/apps/alfred/alfred.sh
@@ -5,10 +5,7 @@ source "$HOME/set-me-up/.dotfiles/utilities/utilities.sh"
 if brew info alfred &>/dev/null; then
 	# install alfred packages
 	npx_install "awm"
-
-	[[ -f "$(locate_alfred_preferences)" ]] && {
-		npx_install "alfred-fkill"
-		npx_install "@bchatard/alfred-jetbrains"
-		npx_install "alfred-npms"
-	}
+	npx_install "alfred-fkill"
+	npx_install "@bchatard/alfred-jetbrains"
+	npx_install "alfred-npms"
 fi
