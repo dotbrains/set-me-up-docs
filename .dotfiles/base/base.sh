3247fd4bf3a2282bac18b8dd6ea802bfea4e1383
Nicholas Adamou <nicholasadamouemail@gmail.com>
2021-11-16 05:05:09 -0500

    added check for rosetta installation

--- .dotfiles/base/base.sh
+++ .dotfiles/base/base.sh
@@ -151,6 +151,40 @@ install_space_vim() {
 
 main() {
 
+	# Installing Rosetta 2 on Apple Silicon Macs
+	# See https://derflounder.wordpress.com/2020/11/17/installing-rosetta-2-on-apple-silicon-macs/
+
+	# Determine OS version
+	# Save current IFS state
+	OLDIFS=$IFS
+
+	IFS='.' read osvers_major osvers_minor osvers_dot_version <<< "$(/usr/bin/sw_vers -productVersion)"
+
+	# restore IFS to previous state
+	IFS=$OLDIFS
+
+	# Check to see if the Mac is reporting itself as running macOS 11
+	if [[ ${osvers_major} -ge 11 ]]; then
+
+		# Check to see if the Mac needs Rosetta installed by testing the processor
+		processor=$(/usr/sbin/sysctl -n machdep.cpu.brand_string | grep -o "Apple")
+
+		if [[ -n $processor ]]; then
+
+			# Check for Rosetta "oahd" process. If not found,
+			# perform a non-interactive install of Rosetta.
+			if ! /usr/bin/pgrep oahd >/dev/null 2>&1; then
+
+				/usr/sbin/softwareupdate --install-rosetta --agree-to-license
+
+			fi
+
+		fi
+
+	fi
+
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 	# We must first create the $HOME/.bash.local configuration file
 	# in order for the brew module to properly install Homebrew.
 	create_bash_local
