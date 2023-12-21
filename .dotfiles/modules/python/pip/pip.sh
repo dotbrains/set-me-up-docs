fd022e5f21700638396c5d9a835adc0b5274527b
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-21 08:24:45 -0500

    imprv pip.sh

--- .dotfiles/modules/python/pip/pip.sh
+++ .dotfiles/modules/python/pip/pip.sh
@@ -11,15 +11,10 @@ declare current_dir && \
 
 install_pip_packages() {
 
-    # Check that pip3 and pipx are installed
+    # Check that pip3 are installed
 
     if ! cmd_exists "pip3"; then
         print_error "pip3 is not installed"
-        exit 1
-    fi
-
-    if ! cmd_exists "pipx"; then
-        print_error "pipx is not installed"
         exit 1
     fi
 
@@ -27,7 +22,7 @@ install_pip_packages() {
 
     # Install pip packages
 
-    pipx install \
+    pip3 install \
         # trash-cli trashes files recording the original path, deletion date, and permissions. 
         # It uses the same trashcan used by KDE, GNOME, and XFCE, but you can invoke it from 
         # the command line (and scripts).
@@ -39,17 +34,6 @@ install_pip_packages() {
         asitop
 }
 
-configure_trash_cli() {
-
-    # Create a directory for the trash-cli
-    # see: https://github.com/andreafrancia/trash-cli#how-to-create-a-top-level-trash-dir
-
-    sudo mkdir --parent /.Trash
-    sudo chmod a+rw /.Trash
-    sudo chmod +t /.Trash
-
-}
-
 main() {
 
     brew_bundle_install -f "brewfile"
@@ -57,10 +41,6 @@ main() {
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     install_pip_packages
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    configure_trash_cli
 
 }
 
